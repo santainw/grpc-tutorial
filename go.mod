f48b2786a159affd0169f19d4cd7ec71a632328f
Thanakon <thanakon.ng@inception.asia>
2020-01-08 16:07:18 +0700

    Server

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/santainw/grpc-tutorial
+
+go 1.12
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/prongbang/grpc-kid v0.0.0-20180809141334-efa985fc39e4
+	github.com/simplesteph/grpc-go-course v0.0.0-20190905085516-f95eaf4be8e5
+	golang.org/x/lint v0.0.0-20190313153728-d0100b6bd8b3 // indirect
+	golang.org/x/tools v0.0.0-20190524140312-2c0ae7006135 // indirect
+	google.golang.org/grpc v1.26.0
+	honnef.co/go/tools v0.0.0-20190523083050-ea95bdfd59fc // indirect
+)
---
+++ server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc/reflection"
+
+	pb "github.com/prongbang/grpc-kid/helloworld/helloworld"
+	"google.golang.org/grpc"
+)
+
+const port = ":50051"
+
+type server struct{}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	log.Printf("Request: %s", in.Name)
+	return &pb.HelloResponse{Message: "Hello " + in.Name}, nil
+}
+
+func (s *server) SayHelloAgain(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{Message: "Hello again " + in.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
